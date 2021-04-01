809b726025188291666cd24a293d971b2f699675
Arshveer0708 <64243859+Arshveer0708@users.noreply.github.com>
2021-03-31 23:14:26 -0400

    Added Staff.cs, Customer.cs, Room.cs

---
+++ SliverSpoon/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SliverSpoon.Models
+{
+    public class Customer
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Customer()
+        {
+            Reservations = new HashSet<Reservation>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Gender { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string ContactNumber { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
